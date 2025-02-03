ee8f5ec7035a152db4bcc10f0bfeec262a1360c7
francis <francisreybetonio46@gmail.com>
2025-02-04 00:05:26 +0800

    added unit tests file

---
+++ tests/com/example/VarTests.java
@@ -0,0 +1,52 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+public class VarTests {
+    @Test
+    void testInputs(){
+        //no initialization
+        String simulatedInput = "int x;\nchar blah;";
+        InputStream in = new ByteArrayInputStream(simulatedInput.getBytes(  ))
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
