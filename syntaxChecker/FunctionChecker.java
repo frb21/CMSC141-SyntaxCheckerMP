b527c367f75faf3e37d9eb857e891b22be5bdf71
francis <francisreybetonio46@gmail.com>
2025-02-05 01:15:01 +0800

    Finished Function checker class

--- syntaxChecker/FunctionChecker.java
+++ syntaxChecker/FunctionChecker.java
@@ -8,6 +8,7 @@ public class FunctionChecker{
     private String input;
     private boolean status;
     private String returnType;
+    private boolean isArgName;
     private String functionType;
     private ArrayList<String> tokens;
     
@@ -16,6 +17,7 @@ public class FunctionChecker{
         input = st;
         status = false;
         returnType = "";
+        isArgName = false;
         tokens = new ArrayList<String>();
         tokenizeString();
         checkSyntax();
@@ -31,7 +33,7 @@ public class FunctionChecker{
     }
 
     private void tokenizeString(){
-        StringTokenizer tokenizer = new StringTokenizer(input, " ,;", true);
+        StringTokenizer tokenizer = new StringTokenizer(input, " ();", true);
 
         while(tokenizer.hasMoreTokens()){
             String tok = tokenizer.nextToken();
@@ -56,13 +58,91 @@ public class FunctionChecker{
                
                 switch(state){
                     //check return type:
+                    case 0: {
+                        if(!(token.equals("int") || token.equals("char") || token.equals("float") || token.equals("double") || token.equals("void"))){
+                            status = false;
+                            System.out.println("function check");
+                            break;
+                        }
 
+                        //returnType = token;
+                        System.out.println("passed return type");
+                        status = true;
+                        ++state;
+                        break;
+                    }
+
+                    //check function naming
+                    case 1:{
+                        if(!(token.charAt(0) == '_' || (token.charAt(0) >= 'a' && token.charAt(0) <= 'z')) || (token.equals("int") || token.equals("char") || token.equals("float") || token.equals("double"))){
+                            status = false;
+                            System.out.println("case1");
+                        }
+                        else if(isArgName){
+                            state = 5;
+                        }
+                        else{
+                            System.out.println("passed function naming");
+                            status = true;
+                            ++state;
+                        }
+                        break;
+                    }
+
+                    //check open parentheses 
+                    case 2:{
+                        if(!token.equals("(")){
+                            status = false;
+                        }
+                        else{
+                            System.out.println("open parent");
+                            status = true;
+                            ++state;
+                        }
+                        break;
+                    }
+
+                    //check argument type
+                    case 3:{
+                        if(!(token.equals("int") || token.equals("char") || token.equals("float") || token.equals("double"))){
+                            status = false;
+                            System.out.println("argument check");
+                        }
+                        else{
+                            System.out.println("passed argument type");
+                            status = true;
+                            ++state;
+                        }
+                        break;
+                    }
+
+                    //check if argument name provided
+                    case 4:{
+                        if(!token.equals(")")){
+                            //go back to state 1 to check valid naming
+                            state = 1;
+                            isArgName = true;
+                        }
+                        else if(token.equals(")")){
+                            System.out.println("closed parenthesis");
+                            status = true;
+                            ++state;
+                        }
+                        break;
+                    }
+
+                    //check semicolon
+                    case 5:{
+                        if(!token.equals(";")){
+                            status = false;
+                        }
+                        System.out.println("passed semicolon");
+                        status = true;
+                        break;
+                    }
                 }
 
             }
-
-
-
         }
     }
 
--- syntaxChecker/Main.java
+++ syntaxChecker/Main.java
@@ -2,8 +2,6 @@ package syntaxChecker;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-
-
 class Main {
     public static void main(String args[]){
         Scanner scanner = new Scanner(System.in);
@@ -12,18 +10,28 @@ class Main {
         for(int i = 0; i < c; i++){
             int type = scanner.nextInt();
             String input = scanner.nextLine();
-            
-            SyntaxChecker syntaxChecker = new SyntaxChecker(input);
-            if(syntaxChecker.getStatus()){
-                System.out.println("VALID");
-            }
-            else{
-                System.out.println("INVALID");
-            }
-        }
-    }
-}
 
+            if(type == 1){
+                SyntaxChecker syntaxChecker = new SyntaxChecker(input);
+                if(syntaxChecker.getStatus()){
+                    System.out.println("VALID");
+                }
+                else{
+                    System.out.println("INVALID");
+                }
+            }
+            else if(type == 2){
+                FunctionChecker funcChecker = new FunctionChecker(input);
+                if(funcChecker.getStatus()){
+                    System.out.println("VALID");
+                }
+                else{
+                    System.out.println("INVALID");
+                }
+            }    
+      }  
+  }
+}
 
 
 
